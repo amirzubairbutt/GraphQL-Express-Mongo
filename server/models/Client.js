8189274e335de21ce0b2b6134d269aeacef166f4
shayan-talha <amirzubairbutt@gmail.com>
2024-05-03 18:57:10 +0500

    Revert "inital Push"
    This reverts commit 6565d74ee752cf7f536c9c487e2dd044112c363a.

--- server/models/Client.js
+++
@@ -1,9 +0,0 @@
-const mongoos = require('mongoose');
-
-const ClientSchema = new mongoos.Schema({
-    name:{type: String},
-    email:{type: String},
-    phone:{type: String},
-});
-
-module.exports = mongoos.model('Client', ClientSchema);
