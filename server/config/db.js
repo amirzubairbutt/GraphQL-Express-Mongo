29bde39380e664928a282a1c36f43375661d45ec
shayan-talha <amirzubairbutt@gmail.com>
2024-05-07 15:56:46 +0500

    Reapply "inital Push"
    This reverts commit 8189274e335de21ce0b2b6134d269aeacef166f4.

---
+++ server/config/db.js
@@ -0,0 +1,8 @@
+const mongoos = require('mongoose');
+
+const connectDB = async () => {
+    const conn = await mongoos.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold)
+}
+
+module.exports = connectDB
